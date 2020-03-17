461018017991489484f5ce1e1055e7606b576e13
Melvywilson <60924199+Melvywilson@users.noreply.github.com>
2020-03-17 12:03:52 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,33 @@
+package com.student;
+
+public class Student implements java.io.Serializable
+{
+private long studentId;
+private String studentName;
+private Address studentAddress;
+public long getStudentId() {
+	return studentId;
+}
+public void setStudentId(long studentId) {
+	this.studentId = studentId;
+}
+public String getStudentName() {
+	return studentName;
+}
+public void setStudentName(String studentName) {
+	this.studentName = studentName;
+}
+public Address getStudentAddress() {
+	return studentAddress;
+}
+public void setStudentAddress(Address studentAddress) {
+	this.studentAddress = studentAddress;
+}
+public Student() {
+}
+public Student(String studentName,Address studentAddress) {
+	this.studentName=studentName;
+	this.studentAddress=studentAddress;	
+}
+}
+
